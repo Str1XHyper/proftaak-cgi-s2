ba81eccb24cd6477f2438fe9b127aa3619935605
i430284@fhict.nl <i430284@fhict.nl>
2020-03-04 12:45:19 +0100

    Created Calendar Page

--- Proftaakrepos/Controllers/HomeController.cs
+++ Proftaakrepos/Controllers/HomeController.cs
@@ -37,6 +37,10 @@ namespace Proftaakrepos.Controllers
         {
             return View();
         }
+        public IActionResult Agenda()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
