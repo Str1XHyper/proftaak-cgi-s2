b913ccbef73ba89ccd257e190c9a388024242b02
BartVerm779 <60880910+BartVerm779@users.noreply.github.com>
2020-03-04 15:50:50 +0100

    Added first part of login-screen

---
+++ Proftaakrepos/Models/LoginModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Proftaakrepos.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Username{ get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password{ get; set; }
+
+    }
+}
