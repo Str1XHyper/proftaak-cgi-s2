69638c78872a49b40b0773b9391e36cd6688d1dc
Tijn van Veghel <tijnvanveghel@gmail.com>
2020-03-04 20:40:01 +0100

    Finished all database connections Shiftview

--- Proftaakrepos/Controllers/HomeController.cs
+++ Proftaakrepos/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Proftaakrepos.Models;
+using MySql.Data.MySqlClient;
 
 namespace Proftaakrepos.Controllers
 {
@@ -50,6 +51,75 @@ namespace Proftaakrepos.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+        public IActionResult HandleRequest(string UserID, int TradeID)
+        {
+
+            MySqlConnection cnn;
+            string connetionString = "server=185.182.57.161;database=tijnvcd415_Proftaak;uid=tijnvcd415_Proftaak;pwd=Proftaak;";
+            cnn = new MySqlConnection(connetionString);
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = cnn;
+            cmd.CommandText = $"Update TradeRequest Set Status = 1 Where TradeId = {TradeID} ";
+            try
+            {
+                cnn.Open();
+                var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                }
+                cnn.Close();
+            }
+            catch (Exception ex)
+            {
+                //"Can not open connection ! " + ex.Message.ToString()
+                return View("ShiftView");
+            }
+
+            cmd.CommandText = $"Update TradeRequest Set UserIdAcceptor = {UserID} Where TradeId = {TradeID}";
+            try
+            {
+                cnn.Open();
+                var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                }
+                cnn.Close();
+            }
+            catch (Exception ex)
+            {
+                //"Can not open connection ! " + ex.Message.ToString()
+                return View("ShiftView");
+            }
+            return RedirectToAction("ShiftView", "Home");
+        }
+
+        public IActionResult Block(string UserID, int TradeID, string DisabledIds)
+        {
+            MySqlConnection cnn;
+            string connetionString = "server=185.182.57.161;database=tijnvcd415_Proftaak;uid=tijnvcd415_Proftaak;pwd=Proftaak;";
+            cnn = new MySqlConnection(connetionString);
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = cnn;
+            cmd.CommandText = $"Update TradeRequest Set DisabledIds = '{UserID} {DisabledIds}'Where TradeId = {TradeID}";
+            try
+            {
+                cnn.Open();
+                var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                }
+                cnn.Close();
+            }
+            catch (Exception ex)
+            {
+                //"Can not open connection ! " + ex.Message.ToString()
+                return View("ShiftView");
+            }
+
+            return RedirectToAction("ShiftView", "Home");
         }
     }
 }
