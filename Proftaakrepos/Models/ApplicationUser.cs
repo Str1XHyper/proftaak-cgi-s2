e1895ab6f8ede31cc08d15d78d54ae0202e50f53
BartVerm779 <60880910+BartVerm779@users.noreply.github.com>
2020-03-05 09:01:02 +0100

    Fixed the login page

--- Proftaakrepos/Models/ApplicationUser.cs
+++ Proftaakrepos/Models/ApplicationUser.cs
@@ -2,7 +2,7 @@
 
 namespace Proftaakrepos.Models
 {
-    public class ApplicationUser : IdentityUser
+    public class ApplicationUser
     {
         public int ID { get; set; }
         public string name { get; set; }
