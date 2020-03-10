ceada0ab0ff869678f8ff9575c431eead4e86081
BartVerm779 <60880910+BartVerm779@users.noreply.github.com>
2020-03-10 07:35:47 +0100

    Fully working and decent looking Login screen added

--- Proftaakrepos/Controllers/AuthenticationController.cs
+++ Proftaakrepos/Controllers/AuthenticationController.cs
@@ -9,19 +9,67 @@ namespace Proftaakrepos.Controllers
 {
     public class AuthenticationController : Controller
     {
+        [HttpGet]
         public IActionResult Login()
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Login(LoginModel model)
+        {
+            int i = 0;
+            string connStr2 = "server=185.182.56.248;user=bartvur381_NGF;database=bartvur381_NGF;password=Bartbart!9";
+            string connStr = "server=185.182.57.161;user=tijnvcd415_Proftaak; database=tijnvcd415_Proftaak;password=Proftaak";
+            MySqlConnection conn = new MySqlConnection(connStr);
+
+            try
+            {
+                conn.Open();
+
+                string sql = $"SELECT `Username`, AES_DECRYPT(Password,'CGIKey')  FROM `Login` WHERE Username='{model.Username.ToLower()}'";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    i++;
+                    if (rdr.GetString(1) == model.Password)
+                    {
+                        return RedirectToAction("Index", "Home"); //redirect to next page
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Wrong e-mail or password");
+                        return View(model);
+                    }
+                }
+                rdr.Close();
+                if (i == 0)
+                {
+                    ModelState.AddModelError("", "Wrong e-mail or password");
+                }
+                else if (i != 1)
+                {
+                    ModelState.AddModelError("", "Multiple emails found, please contact a system administrator.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            conn.Close();
+
+            return View(model);
+        }
         [HttpGet]
         public IActionResult LoginUser()
         {
+            ModelState.AddModelError("", "Wrong e-mail or password");
             return View();
         }
-        public IActionResult AddEmployee()
-        {
-            return View();
-        }
+
         [HttpPost]
         public IActionResult LoginUser(LoginModel model)
         {
