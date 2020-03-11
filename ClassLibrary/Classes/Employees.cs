77d6844930377615e1de4419561c7e9dd15c2408
Bart Vermeulen <bartvermeulen@bartverm.nl>
2020-03-11 22:16:33 +0100

    Added the function to get employee info

---
+++ ClassLibrary/Classes/Employees.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary.Classes
+{
+    public class Employees
+    {
+        SQLConnection sQLConnection = new SQLConnection();
+        private string[] countResults;
+        public int Count()
+        {
+            countResults = sQLConnection.ExecuteSearchQuery("SELECT `UserId` FROM `Werknemers`").ToArray();
+            return countResults.Length;
+        }
+
+        public string[] Names()
+        {
+            return sQLConnection.ExecuteSearchQuery("SELECT `Voornaam`, `Tussenvoegsel`, `Achternaam` FROM `Werknemers`").ToArray();
+        }
+
+        public string[] EmployeeInfo(string name)
+        {
+            return countResults;
+        }
+    }
+}
