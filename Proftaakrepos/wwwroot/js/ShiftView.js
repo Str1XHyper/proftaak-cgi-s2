ca5549aeb962505108aaeb8e284225ab4e430da7
Xander Vos <431852@student.fontys.nl>
2020-03-11 14:14:56 +0100

    Can now see your trade requests

---
+++ Proftaakrepos/wwwroot/js/ShiftView.js
@@ -0,0 +1,24 @@
+﻿//function openCity(cityName) {
+//    var i;
+//    var x = document.getElementsByClassName("city");
+//    for (i = 0; i < x.length; i++) {
+//        x[i].style.display = "none";
+//    }
+//    document.getElementById(cityName).style.display = "block";
+//}
+
+function openCity(evt, cityName) {
+    var i, x, tablinks, status;
+    x = document.getElementsByClassName("city");
+    status = document.getElementById("status");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < x.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " w3-red";
+    status.innerHTML = cityName;
+}
