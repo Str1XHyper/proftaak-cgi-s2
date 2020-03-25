e7b2dbe21249c393e878542e16c25ab26029f18c
Xander Vos <431852@student.fontys.nl>
2020-03-25 12:58:25 +0100

    Fixed Plugin type

--- Proftaakrepos/wwwroot/js/Dashboard.js
+++ Proftaakrepos/wwwroot/js/Dashboard.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     var calendarEl = document.getElementById('calendar');
 
     var calendar = new FullCalendar.Calendar(calendarEl, {
-        plugins: ['googleCalendar'],
+        plugins: ['TimeGrid'],
         googleCalendarApiKey: 'AIzaSyDnLWG_laGq2r92JHi6Smzr3wcMiK_DSps',
         events: {
             googleCalendarId: 'proftaakbde@gmail.com'
