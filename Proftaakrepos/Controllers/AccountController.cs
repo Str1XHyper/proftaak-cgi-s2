d90ac000f5a0ab0decc1305f612da3013f03d35b
Bart Vermeulen <bartvermeulen@bartverm.nl>
2020-04-08 11:15:58 +0200

    Added the posibility to log-out

---
+++ Proftaakrepos/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Proftaakrepos.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult LogOut()
+        {
+            HttpContext.Session.Remove("UserInfo");
+            return RedirectToAction("Login", "Authentication");
+        }
+    }
+}
