c668dda112c96f6c895820da2887d4e2bd2f1653
Bart Vermeulen <bartvermeulen@bartverm.nl>
2020-04-15 20:32:15 +0200

    we're COMMITted

---
+++ ClassLibrary/Classes/ChangePassword.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary.Classes
+{
+    public class ChangePasswordFunc
+    {
+        public bool ChangePass(string currentPassword, string newPassword, string userID)
+        {
+            string currectDBPassword = SQLConnection.ExecuteGetStringQuery($"SELECT AES_DECRYPT(Password,'CGIKey')  FROM `Login` WHERE UserId = '{userID}'")[0];
+            if(currentPassword == currectDBPassword)
+            {
+                Change(newPassword, userID);
+                return true;
+            }
+            return false;
+        }
+
+        private void Change(string newPassword, string userID)
+        {
+            SQLConnection.ExecuteNonSearchQuery($"UPDATE `Login` SET `Password` = AES_ENCRYPT('{newPassword}', 'CGIKey') WHERE `UserId` = '{userID}'");
+        }
+    }
+}
