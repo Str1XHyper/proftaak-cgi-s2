808bf54fb20fa7ecbae3fc7e15697f7798a8ca0a
Tijn van Veghel <tijnvanveghel@gmail.com>
2020-04-15 16:21:30 +0200

    Finished Incidenten and incidenten updates

---
+++ Proftaakrepos/Models/AddStatusUpdateModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Proftaakrepos.Models
+{
+    public class AddStatusUpdateModel
+    {
+        public int IncidentID { get; set; }
+        public int StatusIdIncident { get; set; }
+        public string StatusOmschrijving { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+    }
+}
