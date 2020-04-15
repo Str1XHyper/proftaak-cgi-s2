88ff463c979d37d449a68413628d8a57f22d33d2
Bart Vermeulen <bartvermeulen@bartverm.nl>
2020-04-15 13:57:06 +0200

    latest and the greatest (not)

---
+++ ClassLibrary/Classes/GetIPOfUser.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary.Classes
+{
+    public class GetIPOfUser
+    {
+        public static string GetIpAddressFromHttpRequest(HttpRequest httpRequest)
+        {
+            string ipAddressString = string.Empty;
+            if (httpRequest == null)
+            {
+                return ipAddressString;
+            }
+            if (httpRequest.Headers != null && httpRequest.Headers.Count > 0)
+            {
+                if (httpRequest.Headers.ContainsKey("X-Forwarded-For") == true)
+                {
+                    string headerXForwardedFor = httpRequest.Headers["X-Forwarded-For"];
+                    if (string.IsNullOrEmpty(headerXForwardedFor) == false)
+                    {
+                        string xForwardedForIpAddress = headerXForwardedFor.Split(':')[0];
+                        if (string.IsNullOrEmpty(xForwardedForIpAddress) == false)
+                        {
+                            ipAddressString = xForwardedForIpAddress;
+                        }
+                    }
+                }
+            }
+            else if (httpRequest.HttpContext == null ||
+                 httpRequest.HttpContext.Connection == null ||
+                 httpRequest.HttpContext.Connection.RemoteIpAddress == null)
+            {
+                ipAddressString = httpRequest.HttpContext.Connection.RemoteIpAddress.ToString();
+            }
+            return ipAddressString;
+        }
+    }
+}
