f0d9413e4e6de74868d544b69bba30d4e19c2297
Xander Vos <431852@student.fontys.nl>
2020-05-04 12:32:59 +0200

    Added verlof :)

--- Proftaakrepos/Controllers/VerlofController.cs
+++ Proftaakrepos/Controllers/VerlofController.cs
@@ -1,10 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Proftaakrepos.Controllers
 {
     public class VerlofController : Controller
     {
-        public IActionResult VerlofAanvragen()
+        // GET: Verlof
+        public ActionResult Index()
         {
             return View();
         }
