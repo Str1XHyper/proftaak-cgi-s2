5cb4ef76a1f915c43924e48100ff820d95d51559
Xander Vos <431852@student.fontys.nl>
2020-05-05 19:46:29 +0200

    Different password handeling, almost done :)

---
+++ ClassLibrary/Classes/PasswordCheck.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ClassLibrary.Classes
+{
+    public class PasswordCheck
+    {
+        private int minLen;
+        public bool CheckPassword(string password)
+        {
+            bool number = false;
+            bool specChar = false;
+            bool upper = false;
+            bool lower = false;
+            bool length = false;
+            minLen = 6;
+
+            if (ContainsNumber(password))
+                number = true;
+            if (ContainsSpecialChar(password))
+                specChar = true;
+            if (ContainsUpperCase(password))
+                upper = true;
+            if (ContainsLowerCase(password))
+                lower = true;
+            if (HasMinLenght(password))
+                length = true;
+
+            if (lower && upper && number && specChar && length)
+                return true;
+            else
+                return false;
+        }
+
+        #region Logic
+        private bool ContainsNumber(string password)
+        {
+            Regex regex = new Regex("\\d");
+            return regex.IsMatch(password);
+        }
+
+        private bool ContainsUpperCase(string password)
+        {
+            Regex regex = new Regex("[A-Z]");
+            return regex.IsMatch(password);
+        }
+
+        private bool ContainsLowerCase(string password)
+        {
+            Regex regex = new Regex("[a-z]");
+            return regex.IsMatch(password);
+        }
+
+        private bool ContainsSpecialChar(string password)
+        {
+            Regex regex = new Regex("[#?!@$%^&*-]");
+            return regex.IsMatch(password);
+        }
+
+        private bool HasMinLenght(string password)
+        {
+            int counter = 0;
+            foreach(char c in password)
+            {
+                counter++;
+            }
+            return counter >= minLen;
+        }
+        #endregion
+    }
+}
--- Proftaakrepos/wwwroot/js/passStrength.js
+++ Proftaakrepos/wwwroot/js/passStrength.js
@@ -1,33 +1,72 @@
-function checkPass(input) {
-    var newpass = document.getElementById('newpass1');
-    var newpass2 = document.getElementById("newpass2");
-    var passAlert = document.getElementById("PassAlert");
-    var regex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/g;
-    if (newpass.value.length > 0) {
-        passAlert.style.display = "block";
-    } else {
-        passAlert.style.display = "none";
-        newpass.classList.remove('is-invalid');
-        newpass2.classList.remove('is-invalid');
+//function checkPass(input) {
+//    var newpass = document.getElementById('newpass1');
+//    var newpass2 = document.getElementById("newpass2");
+//    var passAlert = document.getElementById("PassAlert");
+//    var regex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/g;
+//    if (newpass.value.length > 0) {
+//        passAlert.style.display = "block";
+//    } else {
+//        passAlert.style.display = "none";
+//        newpass.classList.remove('is-invalid');
+//        newpass2.classList.remove('is-invalid');
+//    }
+//    if (input == 'pw1') {
+//        if (newpass.value.match(regex)) {
+//            console.log("regex Check")
+//            newpass.classList.add('is-valid');
+//            newpass.classList.remove('is-invalid');
+//        } else {
+//            newpass.classList.remove('is-valid');
+//            newpass.classList.add('is-invalid');
+//        }
+//    } else {
+//        if (newpass.value.match(regex) && newpass.value == newpass2.value) {
+//            console.log("regex Check")
+//            newpass2.classList.add('is-valid');
+//            newpass2.classList.remove('is-invalid');
+//        } else {
+//            newpass2.classList.remove('is-valid');
+//            newpass2.classList.add('is-invalid');
+//        }
+//    }
+
+//}
+
+$("#newpass1").keyup(function () {
+    checkPass($("#newpass1"));
+    if ($("#newpass2").val() != "") {
+        checkIfEqual($("#newpass1"), $("#newpass2"));
     }
-    if (input == 'pw1') {
-        if (newpass.value.match(regex)) {
-            console.log("regex Check")
-            newpass.classList.add('is-valid');
-            newpass.classList.remove('is-invalid');
+});
+
+$("#newpass2").keyup(function () {
+    checkIfEqual($("#newpass1"), $("#newpass2"));
+});
+
+async function checkPass(el) {
+    let status = false;
+    if (el.val() == "")
+        return status;
+    await $.post("../Authentication/CheckPassword", { password: el.val() }, (data) => {
+        if (data) {
+            el.removeClass('is-invalid');
+            el.addClass('is-valid');
+            status = true;
         } else {
-            newpass.classList.remove('is-valid');
-            newpass.classList.add('is-invalid');
+            el.removeClass('is-valid');
+            el.addClass('is-invalid');
+            status = false;
         }
+    });
+    return status;
+}
+
+async function checkIfEqual(el1, el2) {
+    if (el1.val() === el2.val() && await checkPass(el1) === true) {
+        el2.removeClass('is-invalid');
+        el2.addClass('is-valid');
     } else {
-        if (newpass.value.match(regex) && newpass.value == newpass2.value) {
-            console.log("regex Check")
-            newpass2.classList.add('is-valid');
-            newpass2.classList.remove('is-invalid');
-        } else {
-            newpass2.classList.remove('is-valid');
-            newpass2.classList.add('is-invalid');
-        }
+        el2.removeClass('is-valid');
+        el2.addClass('is-invalid');
     }
-    
 }
