4df019dc48f874d316331eb3c8361077b0e99c4a
Alex Peek <I430284@fhict.nl>
2020-05-20 14:03:35 +0200

    Cleaned up code

--- Proftaakrepos/Controllers/PlannerController.cs
+++ Proftaakrepos/Controllers/PlannerController.cs
@@ -10,9 +10,10 @@ using Newtonsoft.Json;
 using Models;
 using Models.Agenda;
 using Proftaakrepos.Authorize;
-using CookieManager;
 using Models.Authentication;
 using Microsoft.AspNetCore.Mvc.Filters;
+using CookieManager;
+using ClassLibrary.Planner;
 
 namespace Proftaakrepos.Controllers
 {
@@ -22,10 +23,11 @@ namespace Proftaakrepos.Controllers
         private static string rol;
         private List<EventModel> eventList;
         private readonly ICookieManager _cookieManager;
+        private AgendaManager agendamanager = new AgendaManager();
+        private EventManager eventmanager = new EventManager();
         public PlannerController(ICookieManager cookieManager)
         {
             _cookieManager = cookieManager;
-
         }
 
         public override void OnActionExecuting(ActionExecutingContext filterContext)
@@ -38,43 +40,14 @@ namespace Proftaakrepos.Controllers
         {
             TempData["CookieMonster"] = _cookieManager.Get<CookieModel>("BIER.User");
             string var = _cookieManager.Get<CookieModel>("BIER.User").Identifier;
-            ViewData["UserInfo"] = var;
-            string[] verlof = null;
-            string[] themeColours = SQLConnection.ExecuteSearchQuery($"select * from ColorScheme").ToArray();
-            string[] loggedUserData = SQLConnection.ExecuteSearchQuery($"Select Rol,UserId From Werknemers Where AuthCode = '{var}'").ToArray();
-            userId = loggedUserData[1];
+            string[] loggedUserData = agendamanager.GetLoggedInUserData(var);
             rol = loggedUserData[0];
-            if (rol.ToLower() == "roostermaker")
-            {
-                verlof = SQLConnection.ExecuteSearchQuery($"Select Count(*) From Verlofaanvragen where Behandeld = '0'").ToArray();
-            }
-            ViewData["colours"] = themeColours;
+            AgendaViewModel viewdata = agendamanager.SetAgendaViewModel(loggedUserData[1]);
+            ViewData["UserInfo"] = var;
+            ViewData["colours"] = agendamanager.GetThemeColours();
+            ViewData["verlof"] = agendamanager.GetVerlofCount(loggedUserData[0]);
             ViewData["rol"] = rol;
-            ViewData["verlof"] = verlof;
-            ViewData["userId"] = userId;
-            AgendaViewModel viewdata = new AgendaViewModel(userId);
-            string[] roosterData = SQLConnection.ExecuteSearchQuery($"Select Rooster.*, Werknemers.Voornaam From Rooster INNER JOIN Werknemers ON Werknemers.UserId = Rooster.UserId").ToArray();
-            List<EventModel> modelList = new List<EventModel>();
-            for (int i = 0; i < roosterData.Length; i += 10)
-            {
-                EventModel model = new EventModel();
-                model.eventId = Convert.ToInt32(roosterData[i]);
-                model.userId = roosterData[i + 1];
-                model.title = roosterData[i + 2];
-                model.description = roosterData[i + 3];
-                model.startDate = Convert.ToDateTime(roosterData[i + 4]);
-                model.endDate = Convert.ToDateTime(roosterData[i + 5]);
-                model.themeColor = roosterData[i + 6];
-                model.isFullDay = Convert.ToInt32(roosterData[i + 7]);
-                model.voornaam = roosterData[i + 9];
-                viewdata.eventList.Add(model);
-            }
-            string[] userData = SQLConnection.ExecuteSearchQuery($"Select UserId, Voornaam, Tussenvoegsel, Achternaam, Rol From Werknemers").ToArray();
-            for (int i = 0; i < userData.Length; i += 5)
-            {
-                UserViewModel usermodel = new UserViewModel(userData[i], userData[i + 1], userData[i + 2], userData[i + 3], userData[i + 4]);
-                viewdata.userList.Add(usermodel);
-            }
+            ViewData["userId"] = loggedUserData[1];
             return View(viewdata);
         }
         #endregion
@@ -161,68 +134,10 @@ namespace Proftaakrepos.Controllers
         public IActionResult FetchAllEvents(string SendUserId)
         {
             string var = HttpContext.Session.GetString("UserInfo");
-            string _userId = "0";
-            string[] userIdArray = new string[0] ;
-            if (SendUserId == null || SendUserId == "0")
-            {
-                _userId = userId;
-            }
-            else
-            {
-                SendUserId = SendUserId.Substring(0, SendUserId.Length - 1);
-                userIdArray = SendUserId.Split(",");
-            }
+            string[] userIdArray = eventmanager.GetSelectedUsersEvents(userId, SendUserId);
+            List<string[]> events = eventmanager.GetEventData(userIdArray);
+            List<EventModel> eventList = eventmanager.FillEventModelList(events, userIdArray, rol);
             ViewData["rol"] = rol;
-            eventList = new List<EventModel>();
-            List<string[]> events;
-            if (SendUserId == "-1")
-            {
-                events = SQLConnection.ExecuteSearchQueryWithArrayReturn($"select Rooster.*, Werknemers.Voornaam from Rooster INNER JOIN Werknemers ON Rooster.UserId = Werknemers.UserId");
-            }
-            else if (SendUserId == "0")
-            {
-                events = SQLConnection.ExecuteSearchQueryWithArrayReturn($"select * from Rooster WHERE UserId = '{_userId}'");
-            }
-            else
-            {
-                string sqlquery = $"select Rooster.*, Werknemers.Voornaam from Rooster INNER JOIN Werknemers ON Rooster.UserId = Werknemers.UserId";
-                for (int i = 0; i < userIdArray.Length; i++)
-                {
-                    if (i == 0)
-                    {
-                        sqlquery += " WHERE ";
-                    }
-                    if (i > 0)
-                    {
-                        sqlquery += " OR ";
-                    }
-                    sqlquery += $"Rooster.UserId = '{userIdArray[i]}'";
-                }
-
-                events = SQLConnection.ExecuteSearchQueryWithArrayReturn(sqlquery);
-            }
-            foreach (string[] e in events)
-            {
-                EventModel em = new EventModel();
-                em.eventId = Convert.ToInt32(e[0]);
-                em.userId = e[1];
-                if (SendUserId == "-1" || SendUserId == null || rol == "Roostermaker")
-                {
-                    em.title = e[9] + " - " + e[2].ToString();
-                }
-                else
-                {
-                    em.title = e[2].ToString();
-                }
-                em.description = e[3].ToString();
-                em.startDate = Convert.ToDateTime(e[4]);
-                em.endDate = Convert.ToDateTime(e[5]);
-                em.themeColor = e[6].ToString();
-                em.isFullDay = Convert.ToInt32(e[7]);
-                em.isPending = Convert.ToBoolean(e[8]);
-                eventList.Add(em);
-            }
-
             ViewData["UserInfo"] = HttpContext.Session.GetString("UserInfo");
             return Json(eventList);
         }
