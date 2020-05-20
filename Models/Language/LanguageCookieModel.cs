c579c909c1b0ebaab8aeb850298c38fbbed03c23
Bart Vermeulen <bartvermeulen@bartverm.nl>
2020-05-20 21:54:01 +0200

    Verlof systeem werkt compleet :)

---
+++ Models/Language/LanguageCookieModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models.Language
+{
+    public class LanguageCookieModel
+    {
+         public string Language { get; set; }
+    }
+}
