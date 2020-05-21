0cb7d3473afeea2d92eaf7712a979d1facc98043
Bart Vermeulen <bartvermeulen@bartverm.nl>
2020-05-21 23:51:13 +0200

    Bij het oplossen van het incident wordt de klant en werknemer op de hoogte gebracht.

--- ClassLibrary/Classes/SendMail.cs
+++ ClassLibrary/Classes/SendMail.cs
@@ -58,22 +58,24 @@ namespace ClassLibrary.Classes
             IRestResponse response = client.Execute(request);
         }
 
-        public static void SendReset(string email)
+        public static void SendEmployeeIncidentSolved(string toEmail, string toName, string incidentmsg, string incidentTitel)
         {
-            List<string[]> info = SQLConnection.ExecuteSearchQueryWithArrayReturn($"SELECT `Voornaam`, `Tussenvoegsel`, `Achternaam`, `Authcode` FROM `Werknemers` WHERE `Email`='{email.ToLower()}'");
-            if(info.Count > 0)
-            {
-                string fullName = string.Empty;
-                if(info[0][1] != string.Empty)
-                {
-                    fullName = info[0][0] + " " + info[0][1] + " " + info[0][2];
-                }
-                else
-                {
-                    fullName = info[0][0] + " " + info[0][2];
-                }
-                //Help(email, fullName, info[0][0], info[0][3]);
-            }
+            var client = new RestClient("https://api.sendgrid.com/v3/mail/send");
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("content-type", "application/json");
+            request.AddHeader("authorization", "Bearer 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");
+            request.AddParameter("application/json", "{\"personalizations\":[{\"to\":[{\"email\":\"" + toEmail + "\",\"name\":\"" + toName + "\"}],\"dynamic_template_data\":{\"klantNaam\":\"" + toName + "\",\"incidentMsg\":\"" + incidentmsg + "\",\"incidentTitle\":\"" + incidentTitel + "\",\"href\":\"https://bier-1.democgi.com/Account/PasswordChange?authCode=" + 5 + "&code=" + 5 + "\"},\"subject\":\"Incident gemeld\"}],\"from\":{\"email\":\"noreply@cgi.com\",\"name\":\"CGI\"},\"reply_to\":{\"email\":\"noreply@cgi.com\",\"name\":\"CGI\"},\"template_id\":\"d-bc568bca3ef74ce1b903390f43093105 \"}", ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+        }
+
+        public static void SendKlantIncidentSolved(string toEmail, string toName, string incidentmsg, string incidentTitel)
+        {
+            var client = new RestClient("https://api.sendgrid.com/v3/mail/send");
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("content-type", "application/json");
+            request.AddHeader("authorization", "Bearer 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");
+            request.AddParameter("application/json", "{\"personalizations\":[{\"to\":[{\"email\":\"" + toEmail + "\",\"name\":\"" + toName + "\"}],\"dynamic_template_data\":{\"klantNaam\":\"" + toName + "\",\"incidentMsg\":\"" + incidentmsg + "\",\"incidentTitle\":\"" + incidentTitel + "\",\"href\":\"https://bier-1.democgi.com/Account/PasswordChange?authCode=" + 5 + "&code=" + 5 + "\"},\"subject\":\"Incident gemeld\"}],\"from\":{\"email\":\"noreply@cgi.com\",\"name\":\"CGI\"},\"reply_to\":{\"email\":\"noreply@cgi.com\",\"name\":\"CGI\"},\"template_id\":\"d-c5db6e3c0a1d4872869f067c598023e9\"}", ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
         }
     }
 }
